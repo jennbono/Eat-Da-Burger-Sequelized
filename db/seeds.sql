aa2eea4d5e61194f6c4404c7a93ed55c1bc5ab5e
Jennifer Bono <jennb24@gmail.com>
2018-01-10 13:31:52 -0600

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+USE burgers_db;
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', false);
